7bc9d662c4332e7c0cd0927b7d73f9fd08aaae15
devansh <devansh@umich.edu>
2023-09-07 22:02:13 -0400

    added a magic number

--- src/unrelated_project/main.cpp
+++ src/unrelated_project/main.cpp
@@ -1,3 +1,9 @@
 #include "myLib.hpp"
+#include <iostream>
 
-int main() { MyLib m("dev"); }
+int main() {
+
+  MyLib m("dev");
+
+  std::cout << "The magic number is " << MAGIC_NUMBER << std::endl;
+}
